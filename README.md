44cbadd822ec68a2bfd17fa4eb6f064a2dbad316
SuperBeppe98 <giuseppebrusco98@gmail.com>
2023-09-02 21:40:24 +0200

    fix

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# convert-12-to-ean13
+# UPC-A Code Generation and Part Update Script
+
+This script retrieves a list of Part objects from InvenTree using its API, generates UPC-A codes based on their EAN-11 codes, and updates the Part objects with the new UPC-A codes. The script is written in Python and utilizes the InvenTree API library.
+
+## Installation
+
+The script requires two external dependencies, `inventree` and `python-dotenv`, which can be installed by running the following command in your terminal or command prompt:
+
+```shell
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the upc_a_generator.py script:
+
+```shell
+python3 upc_a_generator.py
+```
+
+The script will establish a connection to your InvenTree server, authenticate using your provided credentials, and retrieve part data. It then proceeds to sort the parts based on their IPN (International Part Number) and generates and assigns UPC-A codes to each Part object.
+
+Upon completion, the script saves the updated Part objects with the new UPC-A codes.
+
+You can review the generated UPC-A codes in the InvenTree database for each respective Part.
+
+Please ensure that you have the necessary access permissions for your InvenTree server and adhere to the server's usage policies.
+
+Note: The script assumes that the Part class has been correctly imported from the inventree module and that the IPN field exists for each Part object in the InvenTree server's data.
+
+Feel free to customize the script to meet your specific requirements and seamlessly integrate it into your InvenTree workflow.
+
+For any inquiries or issues related to the script, consult the script's documentation or reach out to the script's author.
+
+This README provides comprehensive instructions for installing and utilizing your UPC-A code generation and part update script.
---
+++ upc_a_generator.py
@@ -0,0 +1,73 @@
+from inventree.api import InvenTreeAPI
+from inventree.part import Part
+import re
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+
+SERVER_ADDRESS = os.environ.get('INVENTREE_SERVER_ADDRESS')
+MY_USERNAME = os.environ.get('INVENTREE_USERNAME')
+MY_PASSWORD = os.environ.get('INVENTREE_PASSWORD')
+inventree_api = InvenTreeAPI(SERVER_ADDRESS, username=MY_USERNAME,
+                             password=MY_PASSWORD, timeout=3600)
+
+# Retrieve the list of Part objects using InvenTree API
+parts = Part.list(inventree_api)
+
+# Sort the Part objects based on the new UPC-A code
+sorted_parts = sorted(parts, key=lambda part: part.IPN)
+
+for index, part in enumerate(sorted_parts, start=1):
+    upc_a = part.IPN  # Change variable name to upc_a
+
+    # Ensure that the UPC-A code is exactly 11 digits
+    if len(upc_a) == 11:  # Change comment to UPC-A
+        # Calculate the checksum for the UPC-A code (last digit)
+        total_sum = 0
+
+        for i in range(0, 11):
+            digit = int(upc_a[i])
+            total_sum += digit * (3 if i % 2 == 0 else 1)
+
+        upc_a_checksum = (10 - (total_sum % 10)) % 10
+
+        # Add the checksum to the UPC-A code to get the complete UPC-A code
+        complete_upc_a = upc_a + str(upc_a_checksum)
+
+        # Update the IPN field of the Part object with the new UPC-A code
+        part.save(data={"IPN": complete_upc_a})
+
+        # Print the complete UPC-A code
+        print(f"Complete UPC-A: {complete_upc_a}")  # Change comment to UPC-A
+
+    elif len(upc_a) == 12 and re.match(r'^\d{11}$', upc_a[:11]):
+        # If the IPN is already a valid UPC-A code (11 digits) and ends with '0',
+        # keep it unchanged and print it
+        complete_upc_a = upc_a  # Change comment to UPC-A
+        # Change comment to UPC-A
+        print(f"UPC-A: {upc_a} - Unchanged UPC-A: {complete_upc_a}")
+
+    elif len(upc_a) == 12:
+        # Take the first 11 digits of the UPC-A code
+        upc_a = upc_a[:11]  # Change variable name to upc_a
+
+        # Calculate the checksum for the UPC-A code (last digit)
+        total_sum = 0
+
+        for i in range(0, 11):
+            digit = int(upc_a[i])
+            total_sum += digit * (3 if i % 2 == 0 else 1)
+
+        upc_a_checksum = (10 - (total_sum % 10)) % 10
+
+        # Add the checksum to the UPC-A code to get the complete UPC-A code
+        complete_upc_a = upc_a + str(upc_a_checksum)
+
+        # Update the IPN field of the Part object with the new UPC-A code
+        part.save(data={"IPN": complete_upc_a})
+
+        # Print the complete UPC-A code for each UPC-A input
+        # Change comment to UPC-A
+        print(f"UPC-A: {upc_a} - Complete UPC-A: {complete_upc_a}")
