2ce4004d5c01a85650c15d207bf2b38d2df715d3
SuperBeppe98 <giuseppebrusco98@gmail.com>
2023-09-22 18:36:12 +0200

    fix

--- upc-a-converter.py
+++ upc-a-converter.py
@@ -26,51 +26,33 @@ for index, part in enumerate(sorted_parts, start=1):
     # Get the UPC-A code from the Part object
     upc_a = part.IPN
 
-    # Check if the UPC-A code has 11 digits and is not already correct
-    if len(upc_a) == 11 and not re.match(r'^\d{12}$', upc_a):
+    # Check if the UPC-A code is valid (either 11 or 12 digits)
+    if len(upc_a) == 11 or len(upc_a) == 12:
         total_sum = 0
 
-        # Calculate the UPC-A checksum
-        for i in range(0, 11):
-            digit = int(upc_a[i])
-            total_sum += digit * (3 if i % 2 == 0 else 1)
-
-        upc_a_checksum = (10 - (total_sum % 10)) % 10
-
-        # Append the checksum to the UPC-A code
-        complete_upc_a = upc_a + str(upc_a_checksum)
-
-        # Update the IPN field of the Part object with the new UPC-A code
-        part.save(data={"IPN": complete_upc_a})
-
-        # Print the corrected UPC-A code
-        print(f"Corrected UPC-A: {complete_upc_a}")
-
-    # Check if the UPC-A code has 12 digits and is not already correct
-    elif len(upc_a) == 12 and not re.match(r'^\d{12}$', upc_a):
-        total_sum = 0
-
-        # Calculate the UPC-A checksum
-        for i in range(0, 11):
+        # Calculate the UPC-A checksum using the corrected formula
+        for i in range(0, len(upc_a) - 1):  # Exclude the last digit
             digit = int(upc_a[i])
             total_sum += digit * (3 if i % 2 == 0 else 1)
 
         upc_a_checksum = (10 - (total_sum % 10)) % 10
 
         # Calculate the complete UPC-A code
-        complete_upc_a = upc_a[:11] + str(upc_a_checksum)
+        complete_upc_a = upc_a[:len(upc_a) - 1] + str(upc_a_checksum)
 
-        # Update the IPN field of the Part object with the new UPC-A code
-        part.save(data={"IPN": complete_upc_a})
+        # Check if the computed UPC-A is different from the original
+        if complete_upc_a != upc_a:
+            # Update the IPN field of the Part object with the new UPC-A code
+            part.save(data={"IPN": complete_upc_a})
 
-        # Print the original and corrected UPC-A codes
-        print(f"Original UPC-A: {upc_a} - Corrected UPC-A: {complete_upc_a}")
+            # Print the original and corrected UPC-A codes
+            print(
+                f"Original UPC-A: {upc_a} - Corrected UPC-A: {complete_upc_a}")
+        else:
+            # If the UPC-A code is already correct, skip it
+            print(f"Skipping UPC-A code {upc_a} as it is already correct")
 
-    # If the UPC-A code is already correct or doesn't meet the length criteria, print a message
+    # If the UPC-A code doesn't meet the length criteria, print a message
     else:
-        if len(upc_a) == 11:
-            print(
-                f"Skipping UPC-A code {upc_a} as it is already correct (11 digits)")
-        elif len(upc_a) == 12:
-            print(
-                f"Skipping UPC-A code {upc_a} as it doesn't meet the length criteria (12 digits)")
+        print(
+            f"Skipping UPC-A code {upc_a} as it doesn't meet the length criteria (11 or 12 digits)")
