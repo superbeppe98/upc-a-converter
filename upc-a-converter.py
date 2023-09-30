677a119c559a1bccffd6fd6d2ac52a503c486490
SuperBeppe98 <giuseppebrusco98@gmail.com>
2023-09-30 13:56:19 +0200

    fix 11 digits

--- upc-a-converter.py
+++ upc-a-converter.py
@@ -26,19 +26,19 @@ for index, part in enumerate(sorted_parts, start=1):
     # Get the UPC-A code from the Part object
     upc_a = part.IPN
 
-    # Check if the UPC-A code is valid (either 11 or 12 digits)
-    if len(upc_a) == 11 or len(upc_a) == 12:
+    # Check if the UPC-A code is valid (should be 11 digits)
+    if len(upc_a) == 11:
         total_sum = 0
 
         # Calculate the UPC-A checksum using the corrected formula
-        for i in range(0, len(upc_a) - 1):  # Exclude the last digit
+        for i in range(len(upc_a)):
             digit = int(upc_a[i])
             total_sum += digit * (3 if i % 2 == 0 else 1)
 
         upc_a_checksum = (10 - (total_sum % 10)) % 10
 
-        # Calculate the complete UPC-A code
-        complete_upc_a = upc_a[:len(upc_a) - 1] + str(upc_a_checksum)
+        # Ensure the UPC-A code is 12 digits by adding the checksum
+        complete_upc_a = upc_a + str(upc_a_checksum)
 
         # Check if the computed UPC-A is different from the original
         if complete_upc_a != upc_a:
@@ -48,11 +48,3 @@ for index, part in enumerate(sorted_parts, start=1):
             # Print the original and corrected UPC-A codes
             print(
                 f"Original UPC-A: {upc_a} - Corrected UPC-A: {complete_upc_a}")
-        else:
-            # If the UPC-A code is already correct, skip it
-            print(f"Skipping UPC-A code {upc_a} as it is already correct")
-
-    # If the UPC-A code doesn't meet the length criteria, print a message
-    else:
-        print(
-            f"Skipping UPC-A code {upc_a} as it doesn't meet the length criteria (11 or 12 digits)")
